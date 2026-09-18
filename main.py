c2995ec191b58c1c7f692e26df7dfee150603924
ponnakhu1-jpg <ponnakhu1@gmail.com>
2026-09-18 16:51:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 @app.route("/callback", methods=["POST"])
 def callback():
-signature = request.headers["X-Line-Signature"]
-body = request.get_data(as_text=True)
-try:
-handler.handle(body, signature)
-except InvalidSignatureError:
-abort(400)
-return "OK"
-@handler.add(MessageEvent, message=TextMessage)
+ signature = request.headers["X-Line-Signature"]
+ body = request.get_data(as_text=True)
+ try:
+ handler.handle(body, signature)
+ except InvalidSignatureError:
+ abort(400)
+ return "OK"
+ @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-line_bot_api.reply_message(
-event.reply_token, TextSendMessage(text=event.message.text)
+ line_bot_api.reply_message(
+ event.reply_token, TextSendMessage(text=event.message.text)
 )
 if name == "main":
 app.run()
