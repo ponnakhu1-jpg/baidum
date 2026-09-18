6185588dbffd9ea2fd4aadabe1efd0f532c27d2f
ponnakhu1-jpg <ponnakhu1@gmail.com>
2026-09-18 18:08:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def callback():
     return 'OK'
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event.source.group_id)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=f"คุณส่งข้อความว่า: {event.message.text}")
