4b9e717a7b779dce2c4b3ffd09cf8117f20bf410
ponnakhu1-jpg <ponnakhu1@gmail.com>
2026-09-18 20:15:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def send_lottery_guidance():
     root_numbers=random.sample(range(0,10),2)
     spot_numbers=random.sample(range(0,100),10)
     sets=[random.sample(range(0,1000),6)for _ in range(6)]
-    message_text="เลขรูด:{root_numbers}เลขเจาะ:{spot_numbers}ชุดหวย:{sets}"
+    message_text="nเลขรูด:{root_numbers}nเลขเจาะ:{spot_numbers}fชุดหวย:{sets}"
     random_number=random.randint(10,99)
     line_bot_api.push_message('Ccd80f46ea82114a21319bc2fa4b3b264',messages=TextSendMessage(text=message_text))
 if __name__ == "__main__":
